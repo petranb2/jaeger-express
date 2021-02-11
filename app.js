5c4466f8c504c6409802c44ec8d0cee98554f373
petran <petranb2@gmail.com>
2021-02-11 20:03:22 +0200

    Clean up the middleware

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+const initTracer = require("./middlewares/tracing").initTracer;
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 const jaegerLogger = require("./middlewares/logger");
 var app = express();
-
+const tracer = initTracer("backend-service");
+app.use(function (req, res, next) {
+    req.tracer = tracer;
+    next()
+})
 app.use(logger("dev"));
 app.use(jaegerLogger);
 app.use(express.json());
@@ -16,43 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   console.log(`${req.method} ${req.originalUrl} [STARTED]`);
-//   req.on("end", () => {
-//     console.log(`${req.method} ${req.originalUrl} [end]`);
-//   });
-//   res.on("finish", () => {
-//     console.log(`${req.method} ${req.originalUrl} [FINISHED]`);
-//     req.span.log({
-//       event: "end",
-//       value: "This is the end event",
-//     });
-//     req.span.finish();
-//   });
-
-//   res.on("close", () => {
-//     console.log(`${req.method} ${req.originalUrl} [CLOSED]`);
-//     req.span.log({
-//       event: "end",
-//       value: "This is the end event",
-//     });
-//     req.span.finish();
-//   });
-
-//   next();
-// });
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-// app.use(function (err, req, res, next) {
-//   // logic
-//   console.log("end of the event");
-//   req.span.log({
-//     event: "end",
-//     value: "This is the end event",
-//   });
-//   req.span.finish();
-//   next();
-// });
 module.exports = app;
--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,16 +1,12 @@
-const initTracer = require("./tracing").initTracer;
-
 const jaegerLogger = (req, res, next) => {
-  const tracer = initTracer("backend-service");
-  const span = tracer.startSpan(req.url);
-  span.setTag("hello-to", "logger");
+  const span = req.tracer.startSpan(req.url);
+  span.setTag(req.method, req.path);
   span.log({
     event: "start",
     value: "This is the start event",
   });
   req.span = span;
   res.on("finish", () => {
-    console.log(`${req.method} ${req.originalUrl} [FINISHED]`);
     req.span.log({
       event: "end",
       value: "This is the end event",
