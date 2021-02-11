1898b1189cb71d202d98c87d513669a9686ba00a
petran <petranb2@gmail.com>
2021-02-11 20:06:19 +0200

    Clean up the routes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   console.log("test");
   res.render("index", { title: "Express" });
-  next();
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  // console.log('test')
-  // req.span.log({
-  //   event: "end",
-  //   value: "This is the end event",
-  // });
-  // req.span.finish();
   req.span.log({
     event: "log from route",
     value: "log from route",
   });
   res.send("respond with a resource");
-  // next();
 });
 
 module.exports = router;
